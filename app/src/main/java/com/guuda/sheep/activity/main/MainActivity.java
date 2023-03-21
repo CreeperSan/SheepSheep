60dc84ad7d31d9ef6059567d616ba795fcf84d47
CreeperSan <admin@creepersan.com>
2023-03-22 00:59:18 +0800

    [新增] 应用图标、调整启动页颜色

--- app/src/main/java/com/guuda/sheep/activity/main/MainActivity.java
+++ app/src/main/java/com/guuda/sheep/activity/main/MainActivity.java
@@ -59,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
